f765f87c3bb8ffc3cd265f8497e15e65fe1be95f
khanh.nv <khanh@kybotech.co.uk>
2020-11-02 18:01:05 +0700

    Add project files.

---
+++ BlazorApp_4/Server/Controllers/DeviceTypesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlazorApp_4.Server.Data;
+
+namespace BlazorApp_4.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeviceTypesController : ControllerBase
+    {
+        private readonly Digitalshop_V2Context _context;
+        public DeviceTypesController(Digitalshop_V2Context context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var types = await _context.TblDeviceTypes.ToListAsync();
+            return Ok(types);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Search(string s)
+        {
+            var types = await _context.TblDeviceTypes.Where(t => t.Name.ToLower().Contains(s.ToLower())).ToListAsync();
+            return Ok(types);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var type = new TblDeviceTypes { Typeid = id };
+            _context.Remove(type);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(TblDeviceTypes type)
+        {
+            _context.Add(type);
+            await _context.SaveChangesAsync();
+            return Ok(type.Typeid);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            TblDeviceTypes type = await _context.TblDeviceTypes.FirstOrDefaultAsync(t => t.Typeid == id);
+            return Ok(type);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(TblDeviceTypes type)
+        {          
+            _context.Entry(type).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
